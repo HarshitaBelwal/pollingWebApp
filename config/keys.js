1fbd98d41b603d78f15665772cccef535a94fad4
HarshitaBelwal <belwalharshita@yahoo.in>
2021-05-20 16:16:08 +0530

    main  commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    pusherAppId: '1206079',
+    pusherKey: '8f28be39040bbc14d65e',
+    pusherSecret: '56124ec628045abb2ddc',
+    pusherCluster: 'ap2',
+    pusherEncrypted: true
+  };
